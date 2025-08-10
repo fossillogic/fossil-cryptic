4ce3113e70525825b12d87026473344b195fb1cb
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-08-09 23:21:08 -0600

    mixing in needed guts

---
+++ code/logic/fossil/cryptic/hash.h
@@ -0,0 +1,49 @@
+/*
+ * -----------------------------------------------------------------------------
+ * Project: Fossil Logic
+ *
+ * This file is part of the Fossil Logic project, which aims to develop high-
+ * performance, cross-platform applications and libraries. The code contained
+ * herein is subject to the terms and conditions defined in the project license.
+ *
+ * Author: Michael Gene Brockus (Dreamer)
+ *
+ * Copyright (C) 2024 Fossil Logic. All rights reserved.
+ * -----------------------------------------------------------------------------
+ */
+#ifndef FOSSIL_CRYPTIC_HASH_H
+#define FOSSIL_CRYPTIC_HASH_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+// *****************************************************************************
+// Function prototypes
+// *****************************************************************************
+
+// draft hash algorithm
+
+#ifdef __cplusplus
+}
+#include <stdexcept>
+#include <vector>
+#include <string>
+
+namespace fossil {
+
+namespace cryptic {
+
+
+
+} // namespace cryptic
+
+} // namespace fossil
+
+#endif
+
+#endif /* FOSSIL_CRYPTIC_HASH_H */
