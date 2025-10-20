87925798164f05516eae7dce19ab06807609a7ee
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2025-10-20 18:24:37 -0500

    Update hash.c

--- code/logic/hash.c
+++ code/logic/hash.c
@@ -188,6 +188,7 @@ static uint32_t murmur3_32(const void* key, size_t len, uint32_t seed) {
                 k1 = (k1 << 15) | (k1 >> (32 - 15));
                 k1 *= c2;
                 h1 ^= k1;
+        break;
     }
 
     // finalization
@@ -228,23 +229,25 @@ static uint64_t murmur3_64(const void* key, size_t len, uint32_t seed) {
     const uint8_t* tail = (const uint8_t*)(data + nblocks * 16);
     uint64_t k1 = 0, k2 = 0;
     switch (len & 15) {
-        case 15: k2 ^= ((uint64_t)tail[14]) << 48; break;
-        case 14: k2 ^= ((uint64_t)tail[13]) << 40; break;
-        case 13: k2 ^= ((uint64_t)tail[12]) << 32; break;
-        case 12: k2 ^= ((uint64_t)tail[11]) << 24; break;
-        case 11: k2 ^= ((uint64_t)tail[10]) << 16; break;
-        case 10: k2 ^= ((uint64_t)tail[9]) << 8; break;
-        case 9:  k2 ^= ((uint64_t)tail[8]) << 0; break;
+        case 15: k2 ^= ((uint64_t)tail[14]) << 48;
+        case 14: k2 ^= ((uint64_t)tail[13]) << 40;
+        case 13: k2 ^= ((uint64_t)tail[12]) << 32;
+        case 12: k2 ^= ((uint64_t)tail[11]) << 24;
+        case 11: k2 ^= ((uint64_t)tail[10]) << 16;
+        case 10: k2 ^= ((uint64_t)tail[9]) << 8;
+        case 9:  k2 ^= ((uint64_t)tail[8]) << 0;
                  k2 *= c2; k2 = (k2 << 33) | (k2 >> (64 - 33)); k2 *= c1; h2 ^= k2;
-        case 8:  k1 ^= ((uint64_t)tail[7]) << 56; break;
-        case 7:  k1 ^= ((uint64_t)tail[6]) << 48; break;
-        case 6:  k1 ^= ((uint64_t)tail[5]) << 40; break;
-        case 5:  k1 ^= ((uint64_t)tail[4]) << 32; break;
-        case 4:  k1 ^= ((uint64_t)tail[3]) << 24; break;
-        case 3:  k1 ^= ((uint64_t)tail[2]) << 16; break;
-        case 2:  k1 ^= ((uint64_t)tail[1]) << 8; break;
-        case 1:  k1 ^= ((uint64_t)tail[0]) << 0; break;
+            break;
+        case 8:  k1 ^= ((uint64_t)tail[7]) << 56;
+        case 7:  k1 ^= ((uint64_t)tail[6]) << 48;
+        case 6:  k1 ^= ((uint64_t)tail[5]) << 40;
+        case 5:  k1 ^= ((uint64_t)tail[4]) << 32;
+        case 4:  k1 ^= ((uint64_t)tail[3]) << 24;
+        case 3:  k1 ^= ((uint64_t)tail[2]) << 16;
+        case 2:  k1 ^= ((uint64_t)tail[1]) << 8;
+        case 1:  k1 ^= ((uint64_t)tail[0]) << 0;
                  k1 *= c1; k1 = (k1 << 31) | (k1 >> (64 - 31)); k1 *= c2; h1 ^= k1;
+            break;
     }
 
     // finalization
