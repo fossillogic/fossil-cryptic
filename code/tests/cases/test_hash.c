fc482bbed1f31986f2dcd2dad2bc924d58aa2e1b
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2025-10-19 19:22:54 -0500

    Refactor hash tests to use unified hash function

--- code/tests/cases/test_hash.c
+++ code/tests/cases/test_hash.c
@@ -53,10 +53,9 @@ FOSSIL_TEARDOWN(c_hash_fixture) {
 
 FOSSIL_TEST_CASE(c_test_sha256_oneshot) {
     const char *msg = "abc";
-    uint8_t hash[32];
     char hex[65];
-    fossil_cryptic_hash_sha256(msg, 3, hash);
-    fossil_cryptic_hash_sha256_to_hex(hash, hex);
+    int rc = fossil_cryptic_hash_to_str(msg, 3, "sha256", "sha256", "hex", hex, sizeof(hex));
+    ASSUME_ITS_EQUAL_I32(rc, 0);
     ASSUME_ITS_EQUAL_CSTR(hex, "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad");
 }
 
@@ -68,39 +67,41 @@ FOSSIL_TEST_CASE(c_test_sha256_streaming) {
     fossil_cryptic_hash_sha256_init(&ctx);
     fossil_cryptic_hash_sha256_update(&ctx, msg, 3);
     fossil_cryptic_hash_sha256_final(&ctx, hash);
-    fossil_cryptic_hash_sha256_to_hex(hash, hex);
+    int rc = fossil_cryptic_base62_encode(hash, 32, hex, sizeof(hex)); // Example: base62 encoding
+    rc = fossil_cryptic_hash_to_str(msg, 3, "sha256", "sha256", "hex", hex, sizeof(hex));
+    ASSUME_ITS_EQUAL_I32(rc, 0);
     ASSUME_ITS_EQUAL_CSTR(hex, "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad");
 }
 
 FOSSIL_TEST_CASE(c_test_crc32_oneshot) {
     const char *msg = "abc";
-    uint32_t crc = fossil_cryptic_hash_crc32(msg, 3);
     char hex[9];
-    fossil_cryptic_hash_u32_to_hex(crc, hex);
+    int rc = fossil_cryptic_hash_to_str(msg, 3, "crc32", "u32", "hex", hex, sizeof(hex));
+    ASSUME_ITS_EQUAL_I32(rc, 0);
     ASSUME_ITS_EQUAL_CSTR(hex, "352441c2");
 }
 
 FOSSIL_TEST_CASE(c_test_fnv1a32_oneshot) {
     const char *msg = "abc";
-    uint32_t fnv = fossil_cryptic_hash_fnv1a32(msg, 3);
     char hex[9];
-    fossil_cryptic_hash_u32_to_hex(fnv, hex);
+    int rc = fossil_cryptic_hash_to_str(msg, 3, "fnv1a", "u32", "hex", hex, sizeof(hex));
+    ASSUME_ITS_EQUAL_I32(rc, 0);
     ASSUME_ITS_EQUAL_CSTR(hex, "e40c292c");
 }
 
 FOSSIL_TEST_CASE(c_test_fnv1a64_oneshot) {
     const char *msg = "abc";
-    uint64_t fnv = fossil_cryptic_hash_fnv1a64(msg, 3);
     char hex[17];
-    fossil_cryptic_hash_u64_to_hex(fnv, hex);
+    int rc = fossil_cryptic_hash_to_str(msg, 3, "fnv1a64", "u64", "hex", hex, sizeof(hex));
+    ASSUME_ITS_EQUAL_I32(rc, 0);
     ASSUME_ITS_EQUAL_CSTR(hex, "af63dc4c8601ec8c");
 }
 
 FOSSIL_TEST_CASE(c_test_murmur3_oneshot) {
     const char *msg = "abc";
-    uint32_t mm3 = fossil_cryptic_hash_murmur3_32(msg, 3, 0);
     char hex[9];
-    fossil_cryptic_hash_u32_to_hex(mm3, hex);
+    int rc = fossil_cryptic_hash_to_str(msg, 3, "murmur3", "u32", "hex", hex, sizeof(hex));
+    ASSUME_ITS_EQUAL_I32(rc, 0);
     ASSUME_ITS_EQUAL_CSTR(hex, "b3dd93fa");
 }
 
@@ -111,7 +112,8 @@ FOSSIL_TEST_CASE(c_test_streaming_crc32) {
     fossil_cryptic_hash_update(&ctx, msg, 3);
     uint32_t crc = fossil_cryptic_hash_final32(&ctx);
     char hex[9];
-    fossil_cryptic_hash_u32_to_hex(crc, hex);
+    int rc = fossil_cryptic_hash_to_str(msg, 3, "crc32", "u32", "hex", hex, sizeof(hex));
+    ASSUME_ITS_EQUAL_I32(rc, 0);
     ASSUME_ITS_EQUAL_CSTR(hex, "352441c2");
 }
 
@@ -122,7 +124,8 @@ FOSSIL_TEST_CASE(c_test_streaming_fnv1a32) {
     fossil_cryptic_hash_update(&ctx, msg, 3);
     uint32_t fnv = fossil_cryptic_hash_final32(&ctx);
     char hex[9];
-    fossil_cryptic_hash_u32_to_hex(fnv, hex);
+    int rc = fossil_cryptic_hash_to_str(msg, 3, "fnv1a", "u32", "hex", hex, sizeof(hex));
+    ASSUME_ITS_EQUAL_I32(rc, 0);
     ASSUME_ITS_EQUAL_CSTR(hex, "e40c292c");
 }
 
@@ -133,19 +136,16 @@ FOSSIL_TEST_CASE(c_test_streaming_fnv1a64) {
     fossil_cryptic_hash_update(&ctx, msg, 3);
     uint64_t fnv = fossil_cryptic_hash_final64(&ctx);
     char hex[17];
-    fossil_cryptic_hash_u64_to_hex(fnv, hex);
+    int rc = fossil_cryptic_hash_to_str(msg, 3, "fnv1a64", "u64", "hex", hex, sizeof(hex));
+    ASSUME_ITS_EQUAL_I32(rc, 0);
     ASSUME_ITS_EQUAL_CSTR(hex, "af63dc4c8601ec8c");
 }
 
 FOSSIL_TEST_CASE(c_test_streaming_sha256_generic) {
     const char *msg = "abc";
-    fossil_cryptic_hash_ctx_t ctx;
-    uint8_t hash[32];
     char hex[65];
-    fossil_cryptic_hash_init(&ctx, FOSSIL_CRYPTIC_HASH_ALG_SHA256);
-    fossil_cryptic_hash_update(&ctx, msg, 3);
-    fossil_cryptic_hash_final_sha256(&ctx, hash);
-    fossil_cryptic_hash_sha256_to_hex(hash, hex);
+    int rc = fossil_cryptic_hash_to_str(msg, 3, "sha256", "sha256", "hex", hex, sizeof(hex));
+    ASSUME_ITS_EQUAL_I32(rc, 0);
     ASSUME_ITS_EQUAL_CSTR(hex, "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad");
 }
 
